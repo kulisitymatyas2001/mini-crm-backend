4d54e365aab708694a351a49ec8a8ad98f877d84
Kulisity Mátyás <matyas.kulisity@stud.uni-corvinus.hu>
2026-01-17 14:40:24 +0100

    Add clients router and route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.routers import clients
 
 app = FastAPI()
+
+app.include_router(clients.router, prefix="/clients", tags=["clients"])
 
 @app.get("/")
 def root():
